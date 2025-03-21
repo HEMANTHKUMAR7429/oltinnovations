85d2abbbea67d4fc792ed0c0e1fd6f4a5887c1b9
Hemanth <hemanth7429@gmail.com>
2025-03-21 19:19:52 +0530

    update new 1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="header">
       <div className="header-container">
         <div className="logo">
           <h1>OLT Innovations</h1>
         </div>
-        <nav className="nav-menu">
+        <button 
+          className={`mobile-menu-btn ${isMenuOpen ? 'open' : ''}`}
+          onClick={toggleMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <nav className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
           <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#milestones">Milestones</a></li>
-            <li><a href="#team">Team</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#home" onClick={toggleMenu}>Home</a></li>
+            <li><a href="#about" onClick={toggleMenu}>About</a></li>
+            <li><a href="#projects" onClick={toggleMenu}>Projects</a></li>
+            <li><a href="#milestones" onClick={toggleMenu}>Milestones</a></li>
+            <li><a href="#team" onClick={toggleMenu}>Team</a></li>
+            <li><a href="#contact" onClick={toggleMenu}>Contact</a></li>
           </ul>
         </nav>
       </div>
