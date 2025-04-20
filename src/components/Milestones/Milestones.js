82b935ad1a0bbd3cbdf4392c3a97357650f14d1a
Hemanthkumarputta <86867206+HEMANTHKUMAR7429@users.noreply.github.com>
2025-04-20 18:11:32 +0530

    Update Milestones.js

--- src/components/Milestones/Milestones.js
+++ src/components/Milestones/Milestones.js
@@ -5,12 +5,17 @@ const Milestones = () => {
   const milestones = [
     {
       year: '2024',
-      achievement: 'Received ₹4 Lakhs from Kalpataru CoE, STPI Vizag',
+      achievement: 'Received ₹2.5 Lakhs from Kalpataru CoE, STPI Vizag',
       logo: '/images/kalpataru-logo.png' // Placeholder image path
     },
     {
       year: '2024',
       achievement: 'Secured ₹40,000 from KL University ACIC Incubation Centre',
+      logo: '/images/klu-logo.jpg' // Placeholder image path
+    },
+    {
+      year: '2025',
+      achievement: 'Secured ₹10 Lakhs from IIT BHILAI INNOVATION AND TECHNOLOGY FOUNDATION (IBITF)',
       logo: '/images/klu-logo.jpg' // Placeholder image path
     }
   ];
